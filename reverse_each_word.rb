add962dd3a13e6464df10c93823151d2b7c5d048
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 10:28:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(phrase)
   array = phrase.split(' ')
+  return array
   # result = ""
   # array.each{|string|
   #   result << string.split("").reverse()
   # }
 end
 
-reverse_each_word('Hello World! How Are You?')
+puts reverse_each_word('Hello World! How Are You?')
