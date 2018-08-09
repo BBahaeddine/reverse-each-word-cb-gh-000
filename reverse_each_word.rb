026065447496b7916c146ec5fea57353f2f53df0
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 10:27:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(phrase)
   array = phrase.split(' ')
   result = ""
   array.each{|string|
-    result << string.split("").reverse
+    result << string.split("").reverse()
   }
   puts result
 end
