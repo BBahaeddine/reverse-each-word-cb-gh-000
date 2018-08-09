2ce5a2aa5d6bdc62516eec91e534c9c50c51a397
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 10:33:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
 def reverse_each_word(phrase)
   array = phrase.split(' ')
   result = ""
-  array.each{|string|
+  array.each_with_index{|string, index|
+  last_index = array.size - 1
+  if index == last_index
+    result << "#{string.reverse()}"
+  else
     result << "#{string.reverse()} "
+  end
   }
   return result
 end
